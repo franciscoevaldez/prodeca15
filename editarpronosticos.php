9fb0c755ddeeded8d48f53e261bfd3a6f494afe6
franciscoevaldez <franciscoevaldez@gmail.com>
2015-05-26 11:53:13 -0300

    Comienzo: input 1
    Layout del un partido para input

---
+++ editarpronosticos.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<!-- Basic Page Needs
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<meta charset="utf-8">
+	<title>:: Gamecast :: Prode Copa Am&eacute;rica 2015 ::</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Mobile Specific Metas
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- FONT
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+	<!-- CSS
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/skeleton.css">
+	<link rel="stylesheet" href="css/styles.css">
+
+	<!-- Favicon
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
+<body>
+
+	<!-- Primary Page Layout
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	
+	<header>
+		<h1>Participante</h1>
+		<h2>Editar pronosticos</h2>
+	</header>
+
+	<section id="matchList" class="container">
+
+		<div class="matchGroup row">
+
+			<h3>Grupo A</h3>
+
+
+
+			<article class="match">
+
+				<ul class="teamLine Chile">
+					<li class="icon"></li>
+					<li class="team">Chile</li>
+					<li class="input"><input class="warning" type="text" name="M01-T1" value="?"></li>
+					<li class="data warning">Completar! 3 d&iacute;as</li>
+				</ul>
+
+				<ul class="teamLine Ecuador">
+					<li class="icon"></li>
+					<li class="team">Ecuador</li>
+					<li class="input"><input class="warning" type="text" name="M01-T2" value="?"></li>
+					<li class="data warning">Test</li>
+				</ul>
+
+			</article>
+
+		</div>
+
+	</section>
+
+
+<!-- End Document
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</body>
+</html>
